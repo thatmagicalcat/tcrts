55d6d991370b5c18d5b27ac43a587211dc8e649d
thatmagicalcat <thatmagicalcat@hotmail.com>
2025-04-27 13:06:08 +0530

    added cool macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -47,3 +47,92 @@ fn repeat_internal(
 
     expr
 }
+
+struct ListInput {
+    items: Vec<syn::Type>,
+}
+
+impl syn::parse::Parse for ListInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let mut items = vec![];
+        while let Ok(t) = input.parse::<syn::Type>() {
+            items.push(t);
+            if input.parse::<syn::Token![,]>().is_err() {
+                break;
+            }
+        }
+
+        Ok(Self { items })
+    }
+}
+
+#[proc_macro]
+pub fn list(input: TokenStream) -> TokenStream {
+    let ListInput { mut items } = syn::parse_macro_input!(input as ListInput);
+
+    items.reverse();
+    let mut expr = quote! { Nil };
+
+    for item in items {
+        expr = quote! { Cons<#item, #expr>  };
+    }
+
+    expr.into()
+}
+
+struct ListToArrayInput {
+    list: syn::Type,
+    _comma: syn::Token![,],
+    start: syn::LitInt,
+    _dots: syn::Token![..],
+    eq: Option<syn::Token![=]>,
+    end: syn::LitInt,
+}
+
+impl syn::parse::Parse for ListToArrayInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            list: input.parse()?,
+            _comma: input.parse()?,
+            start: input.parse()?,
+            _dots: input.parse()?,
+            eq: input.parse()?,
+            end: input.parse()?,
+        })
+    }
+}
+
+#[proc_macro]
+pub fn list_to_array(input: TokenStream) -> TokenStream {
+    let ListToArrayInput {
+        list,
+        start,
+        end,
+        eq,
+        ..
+    } = syn::parse_macro_input!(input as ListToArrayInput);
+
+    let start = start.base10_parse::<usize>().unwrap();
+    let end = end.base10_parse::<usize>().unwrap();
+
+    assert!(start < end);
+
+    let range = if eq.is_some() {
+        start..end + 1
+    } else {
+        start..end
+    };
+
+    // types used for indexing
+    let indexing_type = range.map(|i| {
+        repeat_internal(
+            i,
+            quote! { ts_abuse::number::Next },
+            quote! { ts_abuse::number::Zero },
+        )
+    });
+
+    quote! {[
+        #(<#list as ts_abuse::list::GetIndex<#indexing_type>>::Output::VALUE),*
+    ]}.into()
+}
